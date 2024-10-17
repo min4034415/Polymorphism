3957bc25cbfa9c86d94cd6180317379c48c4accb
min4034415 <min4034415@gmail.com>
2024-10-17 12:24:30 +0900

    back to basic

--- Polymorphism/Extensions/Color+Extension.swift
+++ Polymorphism/Extensions/Color+Extension.swift
@@ -18,21 +18,21 @@ extension Color {
         return UIColor(self).toHexString(includeAlpha: includeAlpha)
     }
     
-    func contrastingTextColor() -> Color {
-        // Convert the Color to UIColor to access RGB values
-        let uiColor = UIColor(self)
-        
-        // Extract the red, green, and blue components
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        uiColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
-        
-        // Calculate the brightness (using the luminance formula)
-        let brightness = (red * 0.299 + green * 0.587 + blue * 0.114)
-        
-        // Return white for dark backgrounds and black for light backgrounds
-        return brightness < 0.5 ? .white : .black
-    }
+//    func contrastingTextColor() -> Color {
+//        // Convert the Color to UIColor to access RGB values
+//        let uiColor = UIColor(self)
+//        
+//        // Extract the red, green, and blue components
+//        var red: CGFloat = 0
+//        var green: CGFloat = 0
+//        var blue: CGFloat = 0
+//        uiColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
+//        
+//        // Calculate the brightness (using the luminance formula)
+//        let brightness = (red * 0.299 + green * 0.587 + blue * 0.114)
+//        
+//        // Return white for dark backgrounds and black for light backgrounds
+//        return brightness < 0.5 ? .white : .black
+//    }
     
 }
